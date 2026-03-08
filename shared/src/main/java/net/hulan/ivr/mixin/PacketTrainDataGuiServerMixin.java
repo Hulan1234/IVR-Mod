86ee57e1770ad4b5a5fd34da4d29aa0b87e0ed9f
Hulan1234 <163529385+Hulan1234@users.noreply.github.com>
2026-03-08 16:38:21 +0800

    Delete shared directory

--- shared/src/main/java/net/hulan/ivr/mixin/PacketTrainDataGuiServerMixin.java
+++
@@ -1,9 +0,0 @@
-package net.hulan.ivr.mixin;
-
-
-import mtr.packet.PacketTrainDataGuiServer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value = PacketTrainDataGuiServer.class)
-public class PacketTrainDataGuiServerMixin {
-}
